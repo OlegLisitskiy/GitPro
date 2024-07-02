ce590f0f8db584b9ef3f155e5eb1099a72221b98
OlegMolodec <olegblisitskiy@mail.ru>
2024-07-02 16:19:57 +0300

    del

--- Main.java
+++ Main.java
@@ -1,8 +1,5 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("GitPro");
-        //этот фрагмент из ветки develop
-        System.out.println("lvl1");
-        System.out.println("lvl2");
     }
 }
